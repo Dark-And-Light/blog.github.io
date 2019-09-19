555d9255535ef28ccfb17385045c82d1b4625d96
Dark-And-Light <327271162@qq.com>
2019-09-20 01:40:06 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,10 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   css: {
     sourceMap: true
   },
   //启动运行时编译
-  runtimeCompiler: true,
+  runtimeCompiler: true,// 是否使用包含运行时编译器的 Vue 构建版本
   devServer:{
     open: true,
     host: 'localhost',
@@ -16,4 +15,7 @@ module.exports = {
     // 修复HMR
       config.resolve.symlinks(true);
   },
+  publicPath:'./',
+  outputDir: process.env.outputDir || 'dist', // 'dist', 生产环境构建文件的目录
+  assetsDir: '',  // 相对于outputDir的静态资源(js、css、img、fonts)目录
 }
